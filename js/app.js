2973cb99df3725d2e26ded99b83545b454645af7
Chris Callahan <callahan.c@gmail.com>
2014-10-04 02:26:19 -0400

    build out Espressos and Espresso resources, routes, and templates

--- js/app.js
+++ js/app.js
@@ -4,12 +4,57 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
   this.route( 'about' );
+  this.resource( 'espressos' );
+  this.resource( 'espresso', { path: '/espressos/:name' } );
+});
+
+App.EspressosRoute = Ember.Route.extend({
+  model: function() {
+    return App.ESPRESSOS;
+  }
+});
+
+App.EspressoRoute = Ember.Route.extend({
+  model: function(params) {
+    return App.ESPRESSOS.findBy('name', params.name);
+  }
 });
 
 App.IndexController = Ember.Controller.extend({
   name: 'Chris',
   espresso: './img/espresso.jpg',
   date: function() {
-    return (new Date()).toDateString();
+    return ( new Date() ).toDateString();
   }.property()
 });
+
+App.EspressosController = Ember.Controller.extend({
+
+});
+
+App.ESPRESSOS = [
+  {
+    name: 'Espresso',
+    ingredients: [
+      {
+        type: 'espresso',
+        quantity: 1
+      }
+    ],
+    price: 2.50
+  },
+  {
+    name: 'Macchiato',
+    ingredients: [
+      {
+        type: 'espresso',
+        quantity: 1
+      },
+      {
+        type: 'milk',
+        quantity: 1
+      }
+    ],
+    price: 3.25
+  },
+];
